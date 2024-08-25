0fd647e603048118b0294e80e8c5f298a6321f6d
Luna <luferr98@gmail.com>
2024-08-25 12:31:53 -0300

    Requisitos cumplidos

--- js/script.js
+++ js/script.js
@@ -14,7 +14,7 @@ function showData(dataArray) {
   // El for itera sobre los elementos del array
   for (const item of dataArray) {
     // En la siguiente línea se utilizan "backticks" para armar el String. Más info => https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Template_literals
-    container.innerHTML += `<p> ${item.name} ${item.lastname} ${item.age}</p</p>`; // Se concatena cada párrafo de la manera que queremos mostrarlo al innerHTML del contenedor
+    container.innerHTML += `<p> ${item.name} ${item.lastname}</p</p>`; // Se concatena cada párrafo de la manera que queremos mostrarlo al innerHTML del contenedor
   }
 }
 
